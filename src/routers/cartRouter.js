6ae8e855d478f5cb1faff07f332aa200e17e2ed8
sanefc <san.efc@gmail.com>
2022-04-24 20:17:24 -0300

    finalizado

--- src/routers/cartRouter.js
+++ src/routers/cartRouter.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const cartRouter = express.Router();
 const cartController = require("../controllers/cartController");
+const validateAdmin = require('../middlewares/validateAdmin');
 
-cartRouter.post('/', cartController.create);
-cartRouter.delete('/:id', cartController.delete);
+cartRouter.post('/', validateAdmin, cartController.create);
+cartRouter.delete('/:id', validateAdmin, cartController.delete);
 cartRouter.get('/:id/products', cartController.list);
 cartRouter.post('/:id/products', cartController.addProd);
 cartRouter.delete('/:id/products/:id_prod', cartController.removeProd);
--- src/routers/productRouter.js
+++ src/routers/productRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const productRouter = express.Router();
 const productController = require("../controllers/productController");
+const validateAdmin = require('../middlewares/validateAdmin');
 
 productRouter.get('/:id?', productController.getAll);
-productRouter.post('/', productController.insert); //admin
-productRouter.put('/:id', productController.update); //admin
-productRouter.delete('/:id', productController.delete); //admin
+productRouter.post('/', validateAdmin, productController.insert); //admin
+productRouter.put('/:id', validateAdmin, productController.update); //admin
+productRouter.delete('/:id', validateAdmin, productController.delete); //admin
 
 module.exports = productRouter;
