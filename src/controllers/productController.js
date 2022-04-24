bb43378d77eefaa440966f1c350a436b5698095f
sanefc <san.efc@gmail.com>
2022-04-24 09:22:44 -0300

    Controller productos terminado

--- src/controllers/productController.js
+++ src/controllers/productController.js
@@ -1,5 +1,8 @@
 const Product = require('../models/Product');
 const fs = require('fs');
+const {
+    log
+} = require('console');
 const file = 'src/productsTest.json';
 
 const productController = {
@@ -17,9 +20,49 @@ const productController = {
                 }
             }).catch(e => console.log(e));
     },
-    insert: (req, res) => {},
-    update: (req, res) => {},
-    delete: (req, res) => {}
+    insert: async (req, res) => {
+        await fs.promises.readFile(file, "utf-8")
+            .then(content => {
+                const products = JSON.parse(content);
+                const newProd = new Product({
+                    ...req.body
+                });
+                products.push(newProd);
+                fs.writeFileSync(file, JSON.stringify(products))
+                res.json(products)
+            }).catch(e => console.log(e));
+
+    },
+    update: async (req, res) => {
+        await fs.promises.readFile(file, "utf-8")
+            .then(content => {
+                const products = JSON.parse(content);
+                const index = products.findIndex(p => p.id == req.params.id);
+                if (index != -1) {
+                    products[index] = new Product({
+                        ...req.body
+                    })
+                    fs.writeFileSync(file, JSON.stringify(products))
+                    res.json(products)
+                } else {
+                    res.json('Producto no encontrado.')
+                }
+
+            }).catch(e => console.log(e));
+    },
+    delete: async (req, res) => {
+        await fs.promises.readFile(file, "utf-8")
+            .then(content => {
+                const products = JSON.parse(content);
+                const updatedProds = products.filter(p => p.id != req.params.id);
+                if (products.length != updatedProds.length) {
+                    fs.writeFileSync(file, JSON.stringify(updatedProds))
+                    res.json(updatedProds)
+                } else {
+                    res.json('Producto no encontrado.')
+                }
+            }).catch(e => console.log(e));
+    }
 }
 
 module.exports = productController;
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,5 +1,17 @@
+const {
+    v4: uuidv4
+} = require('uuid');
+
 class Product {
-    constructor(name, description, code, image, price, stock) {
+    constructor({
+        name,
+        description,
+        code,
+        image,
+        price,
+        stock
+    }) {
+        this.id = uuidv4();
         this.name = name;
         this.description = description;
         this.code = code;
